e173b2c16a1738e5851d6f64dc266df7e51dfc78
ngocanh2103iot-cloud <ngocanh2103iot@gmail.com>
2026-03-28 18:50:24 +0700

    +1

--- kitchen/src/main.cpp
+++ kitchen/src/main.cpp
@@ -1,27 +1,28 @@
-#include <Arduino.h>
-#include "config.h"
 #include "audio.h"
+#include "config.h"
 #include "features.h"
-#include "scaler.h"
 #include "inference.h"
+#include "scaler.h"
+#include <Arduino.h>
+
 
 float features[FEATURE_COUNT];
 
 void setup() {
-    Serial.begin(115200);
-    audio_init();
-    model_init();
-    Serial.println("System Started");
+  Serial.begin(115200);
+  audio_init();
+  model_init();
+  Serial.println("System Started");
 }
 
 void loop() {
-    record_audio();
-    extract_features(features);
-    scale_features(features); 
-    
-    int label = run_inference(features);
-    print_result(label);
-    
-    Serial.println("--------------------------");
-    delay(300); 
+  record_audio();
+  extract_features(features);
+  scale_features(features);
+
+  int label = run_inference(features);
+  print_result(label);
+
+  Serial.println("--------------------------");
+  delay(300);
 }
